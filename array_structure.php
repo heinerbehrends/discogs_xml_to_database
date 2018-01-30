129f551ca7211604ce182537dfab0e1c0b918a0e
heinerbehrends <heinerbehrends@hotmail.com>
2018-01-30 10:41:53 +0100

    New xml with just one label. New array_structure.php file.

---
+++ array_structure.php
@@ -0,0 +1,18 @@
+$label = [
+  name => "name",
+  contactinfo => "string",
+  profile => "string",
+  urls => [
+    url =>"#",
+    url => "#"
+  ],
+  sublabels => [
+    label => "A",
+    label => "B",
+    label => "C"
+  ],
+  images => [
+    image => [height => "24", type => "secodary", uri => "#"" ],
+    image => [height => "24", type => "secodary", uri => "#"" ],
+  ],
+]
